c700ebb5afadabaa3afd0e2235a115cb30e191f9
Mhmmdirvn <irvangoblas@gmail.com>
2024-02-20 17:30:39 +0700

    first commit

---
+++ tambahdata.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah Data</title>
+    <style>
+        .row {
+            margin-bottom: 10px;
+
+        }
+    </style>
+</head>
+
+<body>
+    <form action="" method="post">
+        <div class="row">
+            <label for="namaSiswa">Nama Siswa</label>
+            <input type="text" name="namaSiswa" id="">
+        </div>
+
+        <div class="row">
+            <label for="alamat">Alamat</label>
+            <input type="text" name="alamat" id="">
+        </div>
+
+
+        <div class="row">
+            <label for="telepon">Telepon</label>
+            <input type="text" name="telepon" id="">
+        </div>
+
+        <div class="div">
+            <input type="submit" name="simpan" value="Simpan">
+            <a href="tampildata.php">Kembali</a>
+        </div>
+
+    </form>
+</body>
+
+</html>
