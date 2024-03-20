347952379502db9382498c27023084376505420b
Mhmmdirvn <irvangoblas@gmail.com>
2024-03-20 11:52:59 +0700

    add 2 ways about making connection to database

--- config.php
+++ config.php
@@ -1,5 +1,15 @@
 <?php
 
+    //! Cara Pertama 
+
     $koneksi = mysqli_connect('localhost', 'root', '', 'siswa');
 
+    //! Cara Kedua
+
+    $hostname = 'localhost';
+    $username = 'root';
+    $password = '';
+    $databaseName = 'siswa';
+
+    $koneksi = mysqli_connect($hostname, $username, $password, $databaseName);
 ?>
